71e996618dc8d6bdc7e0a04a9b4eeb4fffa8837f
ammeux <38400849+ammeux@users.noreply.github.com>
2018-08-29 15:07:30 +0200

    Add files via upload

---
+++ theBigElephant/StockHistory/stockLoader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace theBigElephant.StockHistory
+{
+    class StockLoader
+    {
+        public List<Stock> LoadStocks()
+        {
+            List<Stock> stockList = new List<Stock>();
+            var doc = new XmlDocument();
+            doc.Load("stocksPortfolio.xml");
+            XmlElement stocks = doc["stocks"];
+            XmlNodeList items = stocks.GetElementsByTagName("stock");
+            foreach (XmlNode item in items)
+                stockList.Add(new Stock()
+                {
+                    Name = item["name"].InnerText,
+                    Symbol = item["symbol"].InnerText
+                });
+            return stockList;
+        }
+    }
+}
