ba6199506e84b23299fc3927d01270a0a91211b4
marco75319 <marc.ammeux@ieseg.fr>
2018-08-30 16:13:12 +0200

    Nunit installed

---
+++ theBigElephant/UnitTests/UnitTestExample.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace theBigElephant.UnitTests
+{   
+    [TestFixture]
+    public class UnitTestExample
+    {
+        [Test]
+        public void CreateStockObject()
+        {
+            Stock stock = new Stock()
+            {
+                Name = "Total",
+                Symbol = "TT"
+            };
+            Assert.AreEqual("Total", stock.Name);
+        }
+    }
+}
